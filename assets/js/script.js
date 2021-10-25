3c9a3289f8b119ba7ef948ffda825410e4f2990d
adairconlin <adair.conlin@gmail.com>
2021-10-25 11:33:20 -0400

    fixed issue with preventDefault() not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,13 @@ let formEl = document.querySelector("#task-form");
 let tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-let createTaskHandler = function(event){
-    event.preventDefault();
+let createTaskHandler = function(e){
+    e.preventDefault();
+
     let listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
-    taskItemEl.className = "task-item";
     tasksToDoEl.appendChild(listItemEl);
 };
 
-formEl.addEventListener("submit", createTaskHandler());
+formEl.addEventListener("submit", createTaskHandler);
