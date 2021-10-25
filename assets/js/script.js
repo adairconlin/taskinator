f0daf337397df74913e90cefc2ba6b0cf803b324
adairconlin <adair.conlin@gmail.com>
2021-10-25 14:24:55 -0400

    add new function and object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,30 @@
 let formEl = document.querySelector("#task-form");
 let tasksToDoEl = document.querySelector("#tasks-to-do");
 
-
-let createTaskHandler = function(e){
-    e.preventDefault();
-    let taskNameInput = document.querySelector("input[name='task-name']").value;
-    let taskTypeInput = document.querySelector("select[name='task-type']").value;
-
+let createTaskEl = function(taskDataObj) {
     let listItemEl = document.createElement("li");   
     listItemEl.className = "task-item";
 
     let taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
  
     listItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(listItemEl);
 };
 
-formEl.addEventListener("submit", createTaskHandler);
+
+let taskFormHandler = function(e){
+    e.preventDefault();
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    let taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    createTaskEl(taskDataObj);
+};
+
+formEl.addEventListener("submit", taskFormHandler);
